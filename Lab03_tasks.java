5259655012ffebcbb3b4c9c26722747f73aa6231
Yaqeen-Aqeel <yaqeenaqeel@hotmail.com>
2024-09-15 15:30:51 +0300

    Update Lab03_tasks.java

--- Lab03_tasks.java
+++ Lab03_tasks.java
@@ -1,5 +1,9 @@
 public class Lab03{
   public static void main(String[] args){
     System.out.println("It works!");
+    int labsNum = 3;
+    for(int i = 1; i <= labsNum; i++){
+      System.out.println("Lab 0" + i + " is done.");
+    }
   }
 }
